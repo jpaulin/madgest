a93cc4a440a4c5f142352abb88a7bcd6b8ad3820
Jukka Paulin <jukka.paulin@gmail.com>
2017-05-02 12:29:58 +0300

    Makes AST from static string using Esprima

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
-var espr = require('esprima');
-
-// Simple AST from a string
-console.log('OK');
+var ep = require('esprima');
 
 // Read in a JS Source file
-//
+
+// Simple AST from a string. 
+//   1st param: String (JavaScript)
+//   2nd param: config [optional]
+//   3rd param: delegate [opt]
+var tree = ep.parse('var x=1;', {}, null);
+
+console.log(tree);
+
+console.log('-----');
+
+// For now, let's make a promise: When console says 'OK', user
+// knows things went well - at least we reached program end.
+console.log('OK');
 
 // Make a syntax tree with Esprima
 
